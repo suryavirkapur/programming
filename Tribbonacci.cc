11b49fbd4e0fd73876dc6973c45fcf57d51ea2f2
Suryavir Kapur <suryavirkapur@hotmail.com>
2026-05-03 14:25:39 +0400

    feat(tribbonacci): add tribbonacci

---
+++ Tribbonacci.cc
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        int t0 = 0;
+        int t1 = 1;
+        int t2 = 1;
+
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        if (n == 2) return 1;
+
+        for (int i = 3; i <= n; ++i) {
+            int curr = t0 + t1 + t2;
+            t0 = t1;
+            t1 = t2;
+            t2 = curr;
+        }
+
+        return t2;
+    }
+};
