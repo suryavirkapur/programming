b21debed4e2f94e7c90f14ca13770bc9630f4aff
Suryavir Kapur <suryavirkapur@hotmail.com>
2026-05-03 14:10:08 +0400

    feat(main): add main

---
+++ main.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <ios>
+
+void solve() {
+}
+
+int main() {
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+
+	int t;
+	std::cin >> t;
+	while (t--) {
+		solve();
+	}
+}
