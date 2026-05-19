bbf88210647729fdb07f2adb1f711b51f26f3efc
Suryavir Kapur <52126281+suryavirkapur@users.noreply.github.com>
2026-05-19 22:47:54 +0400

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        int l1 = s1.size(); int l2 = s2.size();
+        if (l1 > l2) return false;
+        vector<int> v1(26,0);
+        vector<int> v2(26,0);
+
+        for (int i = 0; i < l1; i++)  {
+            v1[s1[i] - 'a']++;
+            v2[s2[i] - 'a']++;
+        }
+        if (v1 == v2) return true;
+        
+
+        for (int i = l1; i < s2.size(); i++) {
+            v2[s2[i - l1] - 'a']--;
+            v2[s2[i] - 'a']++;
+
+            if (v1 == v2) return true;
+        }
+
+        return false;
+    }
+};
