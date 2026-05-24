b4325713c11e72fa1f2ed9915c3378db58fdb0dd
Suryavir Kapur <52126281+suryavirkapur@users.noreply.github.com>
2026-05-24 18:03:49 +0400

    Add: count-paths - submission-0

---
+++ Data Structures & Algorithms/count-paths/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        // since this is an easy DP problem
+        // let's work through it on our own
+
+        // basically what we do is figure out ways to get a point 
+        vector<vector<int>> dp(m, vector<int> (n,0));
+        dp[0][0] = 1;
+        for (int i = 1; i < m; i++) {
+            dp[i][0] = dp[i-1][0];
+        }
+
+        for (int j = 1; j < n; j++) {
+            dp[0][j] = dp[0][j-1]; 
+        }
+
+        for (int i = 1; i < m;  i++) {
+            for (int j = 1; j < n;  j++) {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+
+        return dp[m-1][n-1];
+    }
+};
