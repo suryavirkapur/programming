8702c8f7a6ab6a93780e0b3fc7437cdcdf2c8180
Suryavir Kapur <52126281+suryavirkapur@users.noreply.github.com>
2026-05-28 08:32:43 +0400

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dpa(n);
+        vector<int> dpi(n);
+
+        dpa[0] = nums[0];
+        dpi[0] = nums[0];
+
+        for (int i = 1; i < n; i++) {
+            dpa[i] = max({nums[i], dpa[i-1] * nums[i], dpi[i-1] * nums[i]});
+            dpi[i] = min({nums[i], dpa[i-1] * nums[i], dpi[i-1] * nums[i]});
+        }
+
+        return *max_element(dpa.begin(), dpa.end());
+    }
+};
