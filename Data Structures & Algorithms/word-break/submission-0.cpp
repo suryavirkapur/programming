8072f7d8d21d3d84c9c1d363b2b2ddf1135e1e3b
Suryavir Kapur <52126281+suryavirkapur@users.noreply.github.com>
2026-06-05 20:57:53 +0400

    Add: word-break - submission-0

---
+++ Data Structures & Algorithms/word-break/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+private:
+    bool backtrack(int start, string &s, vector<string>& wordDict, vector<int> &memo) {
+        if (start == s.size()) return true;
+        if (memo[start] != -1) return memo[start];
+        bool ans = false;
+        for (const string &w : wordDict) {
+            if (s.compare(start, w.size(), w) == 0) {
+                if (backtrack(start+w.size(), s, wordDict, memo)) {
+                    memo[start] = 1;
+                    return true;
+                }
+            }
+        }
+        memo[start] = 0;
+        return ans == 1;
+    }
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        vector<int> memo(s.size() + 1, -1);
+        return backtrack(0, s, wordDict, memo);
+    }
+};
