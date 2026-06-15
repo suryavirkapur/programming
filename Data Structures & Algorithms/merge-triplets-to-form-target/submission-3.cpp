ceef65155e606eb889418776da8d16103d7098ba
Suryavir Kapur <52126281+suryavirkapur@users.noreply.github.com>
2026-06-16 00:53:54 +0400

    Add: merge-triplets-to-form-target - submission-3

---
+++ Data Structures & Algorithms/merge-triplets-to-form-target/submission-3.cpp
@@ -0,0 +1,24 @@
+using vi = vector<int>;
+using vii = vector<vector<int>>;
+
+class Solution {
+public:
+    bool mergeTriplets(vector<vector<int>>& triplets, vector<int>& target) {
+        bool hasX = false;
+        bool hasY = false;
+        bool hasZ = false;
+
+        for (auto& t : triplets) {
+            // Reject triplets that would overshoot target
+            if (t[0] > target[0] || t[1] > target[1] || t[2] > target[2]) {
+                continue;
+            }
+
+            if (t[0] == target[0]) hasX = true;
+            if (t[1] == target[1]) hasY = true;
+            if (t[2] == target[2]) hasZ = true;
+        }
+
+        return hasX && hasY && hasZ;
+    }
+};
