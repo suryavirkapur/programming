2d53c8c16ced365d23be412e7932069c26422c95
Suryavir Kapur <suryavirkapur@hotmail.com>
2026-07-07 16:57:03 +0400

    feat(codeforces): add 0000a

---
+++ Codeforces/0000A.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+using std::cin;
+using std::cout;
+using std::vector;
+
+using i64 = long long;
+using vi = vector<int>;
+using vi64 = vector<i64>;
+
+void solve() {
+  i64 n;
+  cin >> n;
+  cout << "" << "\n";
+}
+
+int main(int argc, char *argv[]) {
+  int t;
+  cin >> t;
+  while (t--)
+    solve();
+
+  return 0;
+}
